59060cb2cd29379ec6cede08151be9d3d93440e8
lescudero <lescudero@runaid.com.ar>
2023-08-25 13:56:32 -0300

    Se agrega edición y eliminación de productos

--- models/productosModel.js
+++ models/productosModel.js
@@ -23,4 +23,32 @@ async function setProducto(obj) {
     }
 }
 
-module.exports = {getProductos, setProducto}
+async function deleteProductoById(id) {
+    var query = 'delete from productos where id = ?';
+    var rows = await pool.query(query, [id]);
+    return rows;
+}
+
+async function getProductoById (id) {
+    try{
+        var query = 'select * from productos where id = ?';
+        var rows = await pool.query(query, [id]);
+        return rows[0];
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+async function modProductoById (obj, id) {
+    try{
+        var query = 'update productos set ? where id = ?';
+        var rows = pool.query(query, [obj, id]);
+        return rows;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+module.exports = {getProductos, setProducto, deleteProductoById, getProductoById, modProductoById}
--- routes/admin/productos.js
+++ routes/admin/productos.js
@@ -42,4 +42,48 @@ router.post('/agregar', async (req, res, next) => {
     }
 });
 
+router.get('/eliminar/:id', async (req, res, next) => {
+    var id  = req.params.id;
+    await productosModel.deleteProductoById(id);
+    res.redirect('/admin/productos');
+})
+
+router.get('/modificar/:id', async (req, res, next) => {
+    var id  = req.params.id;
+    var producto = await productosModel.getProductoById(id);
+    console.log(id)
+    console.log(producto)
+    res.render('admin/modificar', {
+    layout: 'admin/layout',
+    producto
+    })
+})
+
+router.post('/modificar', async (req, res, next) => {
+    try {
+        if(req.body.nombre != '' && req.body.precio != 0){
+            var obj = {
+                nombre: req.body.nombre,
+                descripcion: req.body.descripcion,
+                precio: req.body.precio
+            }
+            await productosModel.modProductoById(obj, req.body.id);
+            res.redirect('/admin/productos');
+        } else {
+            res.render('admin/modificar', {
+                layout: 'admin/layout',
+                error: true,
+                message: 'El nombre y el precio del producto es obligatorio.',
+                producto: req.body
+            });
+        }
+    } catch (error) {
+        res.render('admin/modificar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'Ocurrio un error, no se modifico el producto.'
+        });
+    }
+})
+
 module.exports = router;
